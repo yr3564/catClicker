8b0631a58020f45638103db35563bb142fc23efd
Yamil Ruiz <yr3564@att.com>
2018-06-23 09:28:26 -0400

    Added initial cats

--- Documents/Udacity/AJAX_JAVASCRIPT/catclicker/js/app.js
+++ Documents/Udacity/AJAX_JAVASCRIPT/catclicker/js/app.js
@@ -1,10 +1,55 @@
+var initialCats = [
+        {
+            clickCount : 0,
+            name : 'Tabby',
+            imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
+            nicknames : 'TabbyCat'
+        },
+        {
+            clickCount : 0,
+            name : 'Tiger',
+            imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
+            nicknames : 'Tigger'
+        },
+        {
+            clickCount : 0,
+            name : 'Scaredy',
+            imgSrc : 'img/22252709_010df3379e_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709',
+            nicknames : 'scooby'
+
+        },
+        {
+            clickCount : 0,
+            name : 'Shadow',
+            imgSrc : 'img/1413379559_412a540d29_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559',
+            nicknames : 'Blackpanter'
+        },
+        {
+            clickCount : 0,
+            name : 'Sleepy',
+            imgSrc : 'img/9648464288_2516b35537_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288',
+            nicknames : 'zzzzzz'
+
+        }
+
+
+]
+
+
+
+
 // var cat is the model now
-var Cat = function(){
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-	this.imgAttribution = ko.observable('https://wwww.flickr.com/photos/big/');
-	this.nicknames = ko.observable(['tabby', 'kitty', 'peeps', 'meow']);
+var Cat = function(data){
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.imgAttribution = ko.observable(data.imgAttribution);
+	this.nicknames = ko.observable(data.nicknames);
 
 	
 
@@ -34,6 +79,13 @@ var Cat = function(){
 
 var ViewModel = function (){
 	var self = this;
+	// creates the a varaiable to hold the array of initialCats above
+	this.catList = ko.observableArray([]);
+
+	// creates a loop ogf the initialCats data and passed to catList
+	initialCats.forEach( function(catItem){
+		self.catList.push( new Cat (catItem));
+	});
 	//currentCat creates a new cat 
 	this.currentCat = ko.observable( new Cat() );
 	//
